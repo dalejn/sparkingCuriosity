22925268e02c5afd7ccc30a508c7be7bda031970
Dale Zhou <dalejn@gmail.com>
2022-02-02 18:35:19 -0500

    add wiki vectors

--- install.R
+++ install.R
@@ -10,8 +10,9 @@ update.packages(ask = FALSE, instlib = Sys.getenv("R_LIBS_USER"))
 
 #install.packages('/home/jovyan/gganimate-0.1.1.tar.gz', repos = NULL, type = "source")
 #install.packages('/home/jovyan/ggraph-d15fd149babe9ad32316474b9a178e019f376ba6.zip', repos = NULL, type = "source")
-install.packages(c('RColorBrewer', 'igraph', 'ggplot2', 'devtools', 'magick', 'ellipsis', 'vctrs'))
+install.packages(c('RColorBrewer', 'igraph', 'ggplot2', 'devtools', 'magick', 'ellipsis', 'vctrs', 'assertthat', 'backports', 'animation', 'plyr', 'RcppEigen', 'tidyselect', 'polyclip', 'tweenr', 'generics', 'ggforce', 'dplyr'))
 
+library(devtools)
 devtools::install_local('/home/jovyan/gganimate-0.1.1.tar.gz')
 devtools::install_local('/home/jovyan/ggraph-d15fd149babe9ad32316474b9a178e019f376ba6.zip')
 
